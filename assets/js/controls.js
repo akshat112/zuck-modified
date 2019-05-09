6ab2f3a36c69a6e4e26ceac86f97ec1188b9cd23
Akshat Bhanchawat <akshat112@gmail.com>
2019-05-09 18:52:43 +0530

    Back and fwd  button synced with video

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,5 +1,6 @@
 window.orig = 0;
 var traverse = function traverse(pos){
+    window.bar = document.querySelector("div.viewing>div.slides-pointers>div>span.active").getElementsByTagName("b")[0];
     if(window.orig==0){
         window.orig = parseInt(window.bar.style.animationDuration.slice(0,-1)); //original duration
     }
@@ -25,46 +26,39 @@ var traverse = function traverse(pos){
 
 
     } else if(pos=='bwd'){
-        clearInterval(window.increase);
+        // clearInterval(window.increase);
         var ct = window.ie.getElementsByTagName('video')[0].currentTime;
         if(ct>5){
             window.ie.getElementsByTagName('video')[0].currentTime = ct - 5;
+            window.bar.style.animationDuration = (parseInt(window.bar.style.animationDuration.slice(0,-1)) + 5) + 's';
             // window.bar.style.animationDuration = 999999 +'s';
             window.increase = setInterval(movefwd, 0);
             var mw = 0;
             function movefwd() {
-              if (mw >= 100) {
-                clearInterval(window.increase);
-                window.bar.style.animationDuration = 0 +'s';
-              } else {
-                var cp = window.ie.getElementsByTagName('video')[0].currentTime/window.barDuration*100; //current percentage
-                // window.bar.style.minWidth = 0.00001 + '%';
-                window.bar.style.minWidth = cp + '%';
-                window.bar.style.width = cp + '%';
-                mw=cp;
+                if (mw >= 100) {
+                    clearInterval(window.increase);
+                    window.bar.style.animationDuration = 0 +'s';
+                } else {
+                    var cp = window.ie.getElementsByTagName('video')[0].currentTime/window.barDuration*100; //current percentage
+                    // window.bar.style.minWidth = 0.00001 + '%';
+                    window.bar.style.minWidth = 0 + '%';
+                    window.bar.style.width = cp + '%';
+                    mw=cp;
+                }
             }
-        }
-        window.bar.style.animationDuration = (parseInt(window.bar.style.animationDuration.slice(0,-1)) + 5) + 's';
-        } else{
+        } 
+        else{
+            // window.bar.style.animationDuration = (parseInt(window.bar.style.animationDuration.slice(0,-1)))+window.ie.getElementsByTagName('video')[0].currentTime + 's';
             window.ie.getElementsByTagName('video')[0].currentTime = 0.00001;
-            window.increase = setInterval(movefwd, 0);
-            var mw = 0;
-            function movefwd() {
-              if (mw >= 100) {
-                clearInterval(window.increase);
-                window.bar.style.animationDuration = 0 +'s';
-              } else {
-                var cp = window.ie.getElementsByTagName('video')[0].currentTime/window.barDuration*100; //current percentage
-                window.bar.style.minWidth = '';
-                window.bar.style.width = cp + '%'; 
-                mw=cp;
-            }
-        }
-        window.bar.style.animationDuration = (parseInt(window.bar.style.animationDuration.slice(0,-1)))+window.ie.getElementsByTagName('video')[0].currentTime + 's';
+            var pspan = document.querySelector("div.viewing>div.slides-pointers>div>span.active");
+            var theBar = pspan.getElementsByTagName("b")[0];
+            pspan.removeChild(theBar);
+            console.log(theBar);
+            var bTag = "<b style='animation-duration: "+ window.vdo.duration +"s'>";
+            pspan.innerHTML=bTag;
         }
 
     }
-    // clearInterval(window.increase)
 }
 
 // var fwd = function fwd(){
